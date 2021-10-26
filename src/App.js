e3d50a4f2ace33fe93be633e232ee0c5567e7241
Vlad <54101242+VladPobidash@users.noreply.github.com>
2021-10-26 17:52:34 +0300

    Revert "processing by navar (#3)"
    This reverts commit bee26221aefe46e0cd68b30c040197fafd795d3d.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import Home from "./pages/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Category from "./pages/Category";
 
+
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/kits" component={Category} />
-        <Route path="/bras" component={Category} />
-        <Route path="/panties" component={Category} />
-        <Route path="/swimwear" component={Category} />
-        <Route path="/sale" component={Category} />
+     <BrowserRouter>
+     <Navbar />
+     <Switch>
+         <Route path="/" exact component={Home} />
+        <Route path="/kits"  render={(props) => <Category {...props} categoryName="комплекти"/>} />
       </Switch>
-    </BrowserRouter>
+     </BrowserRouter>
   );
 }
 
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,27 +1,22 @@
-import { useRouteMatch } from "react-router";
 import React from "react";
 
-const Category = () => {
-  const route = useRouteMatch();
-  console.log(route);
-  const categoryPath = [
-    { _id: 1, text: "комплекти", path: "/kits" },
-    { _id: 2, text: "бюзгальтери", path: "/bras" },
-    { _id: 3, text: "трусики", path: "/panties" },
-    { _id: 4, text: "купальники", path: "/swimwear" },
-    { _id: 5, text: "SALE", path: "/sale" },
-  ];
-
-  const kkk = categoryPath.map((k) => {
-     if (k.path === route.path) {
-        return k.text
-     }
-  })
-  
+const Category = ({ categoryName }) => {
   return (
-    <>
-      <h1>{kkk}</h1>
-    </>
+     <>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     <div>{categoryName}</div>
+     </>
   );
 };
 
