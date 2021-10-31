8cad2d6d90e4c7fc7ee5db0ff754bac85c7f5c9b
Онофрийчук Максим <onofrijcukmaksim@MacBook-Pro-Onofrijcuk.local>
2021-10-31 20:20:43 +0200

    section instagram

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import FirstScreen from "../components/Home/FirstScreen/FirstScreen";
 import Footer from "../components/Home/Footer/Footer";
 import Sale from "../components/Home/Sale/sale";
+import SectionInstagram from "../components/Home/SectionInstagram/sectionInstagram";
 
 const Home = () => {
   return (
     <div>
       <FirstScreen />
       <Sale />
+      <SectionInstagram />
       <Footer />
     </div>
   );
