100589a80e3483544eab14178d247a874c51825a
Онофрийчук Максим <onofrijcukmaksim@MacBook-Pro-Onofrijcuk.local>
2021-11-15 17:44:49 +0200

    props using

--- src/components/Category/CategoryCard.jsx
+++ src/components/Category/CategoryCard.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Tabs, Tab } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import ComplectPhoto from "../../assets/card-sale-3.jpg";
 
-const CategoryCard = (card) => {
+const CategoryCard = ({ card }) => {
   return (
     <div className="category__card">
-      <img src={ComplectPhoto} alt="category__img" className="category__img" />
+      <img src={card.photo} alt="category__img" className="category__img" />
       <div className="card__bottom">
         <p className="card__name">{card.name}</p>
         <span className="card__price">{card.price}</span>
