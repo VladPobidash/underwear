f1b681568df116cdc3f07bd71b575311723410e3
Онофрийчук Максим <onofrijcukmaksim@MacBook-Pro-Onofrijcuk.local>
2021-11-15 17:32:37 +0200

    Renaming

--- src/components/Home/OurCategories/OurCategories.jsx
+++ src/components/Home/OurCategories/OurCategories.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Tabs, Tab } from "@mui/material";
 import CardSale1 from "../../../assets/card-sale-1.jpg";
-import "./categoriesCard.scss";
+import "./ourCategories.scss";
 
-const CategoriesCard = () => {
-  const CategoriesCardStorage = [
+const OurCategories = () => {
+  const OurCategoriesStorage = [
     { id: 1, name: "Купальники", photo: CardSale1 },
     { id: 2, name: "Бюстгалтери", photo: CardSale1 },
     { id: 3, name: "Трусики", photo: CardSale1 },
     { id: 4, name: "Комплекты", photo: CardSale1 },
   ];
   return (
-    <section className="categoriesCard">
-      <div className="container categoriesCard__container">
-        <span className="categoriesCard__title">Categories</span>
-        <div className="categoriesCard__wrapper">
-          {CategoriesCardStorage.map((cat) => {
+    <section className="our_categories">
+      <div className="container ourCategories__container">
+        <span className="our_categories__title">Categories</span>
+        <div className="our_categories__wrapper">
+          {OurCategoriesStorage.map((cat) => {
             return (
-              <div className="categoriesCard__card">
+              <div className="our_categories__card">
                 <img
-                  className="categoriesCard__img"
+                  className="our_categories__img"
                   src={cat.photo}
-                  alt="categoriesCard-img"
+                  alt="our_categories-img"
                 />
-                <div className="categoriesCard__bottom">
-                  <div className="categoriesCard__text">{cat.name}</div>
+                <div className="our_categories__bottom">
+                  <div className="our_categories__text">{cat.name}</div>
                   <Tabs>
                     <Tab
                       sx={{
@@ -35,7 +35,7 @@ const CategoriesCard = () => {
                         borderRadius: "10px",
                         padding: "3px",
                       }}
-                      className="categoriesCard__btn"
+                      className="our_categories__btn"
                       label="Перейти"
                       to="/allCategories"
                       component={Link}
@@ -51,4 +51,4 @@ const CategoriesCard = () => {
   );
 };
 
-export default CategoriesCard;
+export default OurCategories;
